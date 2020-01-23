f4bee891d70ed7ef4b68aff1204cceadf22e0b0e
unknown <bettercallmiguel@gmail.com>
2020-01-23 17:35:08 -0300

    Criando a pasta posição

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using tabuleiro;
 
 namespace Xadrez
 {
@@ -10,6 +11,13 @@ namespace Xadrez
     {
         static void Main(string[] args)
         {
+            Posicao P;
+
+            P = new Posicao(3, 4);
+
+            Console.WriteLine("Posição: "+P);
+
+            Console.WriteLine();
         }
     }
 }
