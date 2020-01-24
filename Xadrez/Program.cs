0718ca489482d74fe44920ad7dbe6a720cd34aa4
unknown <bettercallmiguel@gmail.com>
2020-01-24 15:36:33 -0300

    Comecando a colocar pecas no tabuleiro

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using tabuleiro;
+using Xadrez;
 
 namespace Xadrez
 {
@@ -12,6 +13,11 @@ namespace Xadrez
         static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
+
+            tab.colocarPeca(new Torre(tab,Cor.Preta), new Posicao(0, 0));
+            tab.colocarPeca(new Torre(tab , Cor.Preta), new Posicao(1, 3));
+            tab.colocarPeca(new Rei(tab , Cor.Preta), new Posicao(2, 4));
+
             Tela.imprimirTabuleiro(tab);
             Console.WriteLine( );
 
--- Xadrez/Tela.cs
+++ Xadrez/Tela.cs
@@ -14,11 +14,11 @@ namespace Xadrez
             for (int i = 0; i<tab.linhas; i++)
             {
                 for (int j = 0; j<tab.colunas; j++)
-                {
+                { //Se o tabuleiro não possuir peças...
                     if (tab.peca(i, j) == null)
                     {
                         Console.Write("- ");
-                    }
+                    }//Se o tabuleiro possuir peças , imprima a peça na posição , e dê um espaço.
                     else
                     {
                         Console.Write(tab.peca(i,j)+" ");
