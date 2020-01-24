a06b7edb6c256f2ad9496224601f2efa2df29549
unknown <bettercallmiguel@gmail.com>
2020-01-24 14:56:50 -0300

    Comecando a implementar metodo que imprime o tabuleiro

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -12,8 +12,9 @@ namespace Xadrez
         static void Main(string[] args)
         {
             Tabuleiro tab = new Tabuleiro(8, 8);
-
+            Tela.imprimirTabuleiro(tab);
             Console.WriteLine( );
+
         }
     }
 }
--- Xadrez/tabuleiro/Tabuleiro.cs
+++ Xadrez/tabuleiro/Tabuleiro.cs
@@ -19,5 +19,10 @@ namespace tabuleiro
             pecas = new Peca[linhas, colunas];
         }
 
+        public Peca peca (int linha , int coluna)
+        {
+            return pecas[linha, coluna];
+        }
+
     }
 }
