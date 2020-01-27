7a6641c11293cd8b1931383220750903759ed11f
unknown <bettercallmiguel@gmail.com>
2020-01-27 17:28:37 -0300

    Melhorando a implementacao do metodo Imprimir Tabuleiro

--- Xadrez/Tela.cs
+++ Xadrez/Tela.cs
@@ -13,6 +13,7 @@ namespace Xadrez
         {
             for (int i = 0; i<tab.linhas; i++)
             {
+                Console.Write(8 -i + " ");
                 for (int j = 0; j<tab.colunas; j++)
                 { //Se o tabuleiro não possuir peças...
                     if (tab.peca(i, j) == null)
@@ -21,11 +22,29 @@ namespace Xadrez
                     }//Se o tabuleiro possuir peças , imprima a peça na posição , e dê um espaço.
                     else
                     {
-                        Console.Write(tab.peca(i,j)+" ");
+                        imprimirPeca(tab.peca(i, j));
+                        Console.Write(" ");
                     }
                     
                 }
-                Console.WriteLine();
+                Console.WriteLine(" ");
+                
+            }
+            Console.WriteLine("  a b c d e f g h");
+        }
+
+        public static void imprimirPeca(Peca peca)
+        {
+            if (peca.cor == Cor.Branca)
+            {
+                Console.Write(peca);
+            }
+            else
+            {
+                ConsoleColor aux = Console.ForegroundColor;
+                Console.ForegroundColor = ConsoleColor.Yellow;
+                Console.Write(peca);
+                Console.ForegroundColor = aux;
             }
         }
     }
