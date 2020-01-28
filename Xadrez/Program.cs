292eda97a0551c438211e4aeda6eaa9d552b788a
unknown <bettercallmiguel@gmail.com>
2020-01-28 11:55:56 -0300

    Fazendo os primeiros testes de movimentacao

--- Xadrez/Program.cs
+++ Xadrez/Program.cs
@@ -14,15 +14,23 @@ namespace Xadrez
         {
             try
             {
-                Tabuleiro tab = new Tabuleiro(8, 8);
+                PartidaDeXadrez partida = new PartidaDeXadrez();
+                while (!partida.terminada)
+                {
+                    Console.Clear();
+                    Tela.imprimirTabuleiro(partida.tab);
+                    Console.WriteLine();
+                    Console.Write("Origem: ");
+                    Posicao origem = Tela.lerPosicaoXadrez().toPosicao();
+                    Console.Write("Destino: ");
+                    Posicao destino = Tela.lerPosicaoXadrez().toPosicao();
 
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(0, 0));
-                tab.colocarPeca(new Torre(tab, Cor.Preta), new Posicao(1, 3));
-                tab.colocarPeca(new Rei(tab, Cor.Preta), new Posicao(0, 2));
+                    partida.executaMovimento(origem, destino);
 
-                tab.colocarPeca(new Torre(tab, Cor.Branca), new Posicao(3, 5));
-                Tela.imprimirTabuleiro(tab);
-                Console.WriteLine();
+                    
+
+                }
+                
             }
             catch (TabuleiroException e)
             {
--- Xadrez/tabuleiro/Peca.cs
+++ Xadrez/tabuleiro/Peca.cs
@@ -22,5 +22,10 @@ namespace tabuleiro
 
         }
 
+        public void incrementarQteMovimentos()
+        {
+            QteMovimentos++;
+        }
+
     }
 }
