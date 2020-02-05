70c9c4b082fe248e4611fe80fc3907a01332ae99
unknown <bettercallmiguel@gmail.com>
2020-02-05 18:24:54 -0300

    jogada especial promocao

--- Xadrez/Xadrez/PartidaDeXadrez.cs
+++ Xadrez/Xadrez/PartidaDeXadrez.cs
@@ -149,6 +149,21 @@ namespace Xadrez
                 desfazMovimento(origem, destino, pecaCapturada);
                 throw new TabuleiroException("Você não pode realizar uma jogada que te deixa em cheque!");
             }
+            Peca p = tab.peca(destino);
+
+            //#jogadaespecial promocao
+            if (p is Peao)
+            {
+                if (p.cor == Cor.Branca && destino.linha == 0 || (p.cor == Cor.Preta && destino.linha == 7))
+                {
+                    p = tab.retirarPeca(destino);
+                    pecas.Remove(p);
+                    Peca dama = new Dama(tab, p.cor);
+                    tab.colocarPeca(dama, destino);
+                    pecas.Add(dama);
+                }
+            }
+
 
             if (estaEmXeque(adversaria(jogadorAtual)))
             {
@@ -167,9 +182,7 @@ namespace Xadrez
             {
                 turno++;
                 mudaJogador();
-            }
-
-            Peca p = tab.peca(destino);
+            }           
 
             //#jogadaespecial En Passant
             if(p is Peao && (destino.linha == origem.linha -2 || destino.linha == origem.linha + 2))
